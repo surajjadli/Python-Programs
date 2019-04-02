d7e17268255eb747e8e888156859741870633627
suraj jadli <37240902+surajjadli@users.noreply.github.com>
2019-04-02 07:51:27 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,4 +1,10 @@
+# python program to print  following things
+# this is \\\\ double backslash
+# these are /\\/\\/\\/\\ mountains
+# he is   awesome
+# \" \n \t \'
+
 print("this is \\\\ double backslash")
 print("these are /\\/\\/\\/\\ mountains")
-print("he is \t is awesome")
+print("he is \t  awesome")
 print("\\\" \\n \\t \\'")
