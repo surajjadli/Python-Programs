994f00a2a3de5c77652f72bc855f1a29cae2ee97
suraj jadli <surajjjadli3@gmail.com>
2019-04-05 10:18:22 +0530

    forteen file

---
+++ ex14.py
@@ -0,0 +1,11 @@
+def rev_lst_itm(l):
+    new_lst = []
+    for i in l:
+        new_lst.append(i[::-1])
+        
+    return new_lst
+
+lst = ['abc', 'tuv', 'xyz']
+
+print(lst)
+print(rev_lst_itm(lst))
